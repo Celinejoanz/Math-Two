c5cfefb9fd94a40dc3483b8798cf39cc23376733
Chidimma Uba <chidimmajuba@gmail.com>
2018-05-31 15:10:06 -0400

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String my_number;
+        my_number ="2";
+        String my_other_number;
+        my_other_number = "1.7938";
+        System.out.println(my_number +" "+my_other_number);
+
+    }
+}
